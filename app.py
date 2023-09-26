2a8f2fc093a5b594161dd25f33e2b634de72dd07
cato <catocarling@gmail.com>
2023-09-26 16:05:41 -0400

    login

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
+colmn1 , colmn2 , colmn3 = st.columns(3)
 
-st.subheader ("numero ono")
-st.caption("numero dos")
-st.header("numero tres")
-st.title("numero cuatro")
-st.text("numero cinco")
 
+username = colmn1.text_input("username")
+# st.write(username)
+password = colmn2.text_input ("password")
 sidebar = st.sidebar
-sidebar.title("this app is better than any other")
+button = colmn1.button ("login")
