3d240e1bc5758fffe51b3818685f92504ba8c9f1
cato <catocarling@gmail.com>
2024-01-09 15:19:35 -0500

    hello

--- app.py
+++ app.py
@@ -24,35 +24,18 @@ def set_png_as_page_bg(png_file):
     
     st.markdown(page_bg_img,unsafe_allow_html=True)
 
-<<<<<<< HEAD
-set_png_as_page_bg("img/bg.png")
-=======
 set_png_as_page_bg("img/christmas_background.jpg")
->>>>>>> 78b69e9b02c088daa2c92642738c67e59826a0f4
 #********************************************************************
 #login form
 
 username = st.text_input("username",label_visibility="hidden",placeholder="Username")
 # st.write(username)
-<<<<<<< HEAD
-password = st.text_input ("password",label_visibility="hidden",placeholder="Password")
-=======
 password = colmn2.text_input ("password",label_visibility="hidden",placeholder="Password")
 # st.write(password)
->>>>>>> 78b69e9b02c088daa2c92642738c67e59826a0f4
 button_loging = st.button ("login")
 
 #sidebar app
 with st.sidebar:
     button(username='catocarling',floating=False)
 
-helo = st.text_input ("enter an number") 
-mom = st.text_input ("enter a second number")
 
-st.title(helo)
-st.title(mom)
-
-def hi (n,m):
-    st.title(n+m)
-    
-hi (helo,mom)
